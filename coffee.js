401cb5e45fffc0b139c01955dfda8353d59d8cac
lerover1155@gmail.com <lerover1155@gmail.com>
2023-03-08 19:13:19 +0630

    firstAndSmallApp

--- coffee.js
+++ coffee.js
@@ -1,5 +1,9 @@
-const countEl = document.querySelector('.count')
+let customerCount = []
 
+const countEl = document.querySelector('.count')
+let greeting = 'Good Moring'
+let customerName = 'Win'
+const titleEl = document.querySelector('.greeting')
  
 let count = 0
 countEl.innerText = count
@@ -13,6 +17,15 @@ function decrease() {
     countEl.innerText = count
 }
 
+function save() {
+    customerCount.push(count)
+    console.log(customerCount)
+   
+}
+
+function greet() {
+    titleEl.innerText = greeting + ' ' + customerName
+}
 
 
 
